1efba2e215c6cfb9313ace078e36233e100daed3
ishii-htm <ishii@hitomedia.jp>
2015-03-02 16:07:52 +0900

    add meshi

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -2,7 +2,8 @@ package controllers
 
 import "github.com/revel/revel"
 import "strings"
-
+import "math/rand"
+import "time"
 type App struct {
 	*revel.Controller
 }
@@ -27,9 +28,36 @@ func (c App) Incoming(
   text string,
   trigger_word string) revel.Result {
 
+		var response JsonResponse
+
   // TODO トークンのチェックぐらいはするべきですよ
 
-  response := JsonResponse{strings.TrimSpace(strings.Replace(text, trigger_word, "", -1))  + "よねー　わかるー", "socrates-go"}
+	// trigger はいらない子なので殺す
+	text = strings.TrimSpace(strings.Replace(text, trigger_word, "", -1))
+
+	// メッセージをバラして
+	fields := strings.Fields(text)
+
+	switch strings.TrimSpace(fields[0]) {
+	case "meshi":
+		// マッチしたらめし
+		response = c.Meshi()
+	default :
+		// マッチしないやつにはとりあえず共感しとく(クズっぽさある)
+	  response = c.Empathy(text, trigger_word)
+	}
 
   return c.RenderJson(response)
+}
+
+func (c App) Empathy(msg string, trigger string) JsonResponse {
+	return JsonResponse { msg + "よねー　わかるー", "socrates-go" }
+}
+
+func (c App) Meshi() JsonResponse {
+	meshi_list := []string{ "麻布ラーメン", "鶴見屋", "謎のカレー", "GOGOカレー", "大戸屋", "AJITO", "豚組", "新規開拓" }
+
+	rand.Seed(time.Now().Unix())
+
+	return JsonResponse { meshi_list[rand.Intn(len(meshi_list))] + "のめしー", "socrates-go" }
 }
